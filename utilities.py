7502bb54731db62fb089ee60864aa63277e51eb3
Brian Spiering <bspiering@gmail.com>
2018-12-01 06:57:24 -0800

    Solution to Day 1

---
+++ utilities.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Helper functions for Advent of Code
+"""
+
+from typing import List
+
+def load_input(day: int) -> List[str]:
+    with open(f'./input/day_{day}.txt') as f:
+        data = f.read().splitlines()
+    return data
